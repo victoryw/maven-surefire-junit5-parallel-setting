aa565280dea772e4f912d7c73b2536c6aed4d882
victoryw <tianya_wy@hotmail.com>
2022-11-30 15:41:36 +0800

    [victoryw] init

---
+++ src/test/java/com/victoryw/maven/surefire/junit5/parallel/setting/MavenSurefireJunit5ParallelSettingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victoryw.maven.surefire.junit5.parallel.setting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenSurefireJunit5ParallelSettingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
